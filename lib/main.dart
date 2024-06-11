64069e9859d7ca3ae3e5d649db3caaffe980e3bd
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-06-11 21:38:20 +0530

    created a Expense class for storing data of each item

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:expensive_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: ,
+    const MaterialApp(
+      home: Expenses(),
     ),
   );
 }
---
+++ lib/models/expense.dart
@@ -0,0 +1,16 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Expense {
+  Expense({
+    required this.date,
+    required this.title,
+    required this.amount,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
